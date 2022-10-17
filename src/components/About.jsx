cf3b43b8c5ef9f0cf0917edd2ea25446f2c132d0
Joey Faris <joeyfaris1@yahoo.com>
2022-10-17 12:31:36 -0700

    about edits

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,12 +8,12 @@ const About = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
             </div>
                 <p className='text-xl mt-20'>
-                I am a passionate and highly motivated full stack developer with experience using frontend and backend technologies such as Javascript, React, Node, Bootstrap, CSS (Tailwind CSS & Bootstrap), Ruby on Rails, Sinatra, Active Record, SQL and PostgreSQL. I love to build new projects and debugging. I am also committed to writing clean, readable, and scalable code.
+                I am a passionate and highly motivated full stack developer with experience using frontend and backend technologies such as Javascript, React, Node, Bootstrap, CSS (Tailwind CSS & Bootstrap), Ruby on Rails, Sinatra, Active Record, SQL and PostgreSQL. I am also committed to writing clean, readable, and scalable code.
                 </p>
                 <br />
 
                 <p className='text-xl'>
-                I received my Bachelor's from Cal Poly San Luis Obispo in 2019 where I shortly after I worked in Real Estate and Sales. I wanted to change career paths into software engineering, where I recently graduated from Flatiron School's full stack software engineering boot camp. I would love to apply my problem solving and programming skills to help clients and companies develop and maintain their software and technology.</p>
+                I received my Bachelor's from Cal Poly San Luis Obispo in 2019 where I shortly after I worked in Real Estate and Sales. I wanted to change career paths into software engineering, where I recently graduated from Flatiron School's full stack software engineering boot camp. I would love to apply my analytical, technical and programming skills to help clients and companies develop and maintain their software and technology.</p>
         </div>
     </div>
   )
