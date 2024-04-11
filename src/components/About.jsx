c5e94928220f86f44a8c72b6995bce79c55fe13d
Joey Faris <joeyfaris1@yahoo.com>
2024-04-11 10:58:02 -0700

    changed about sec

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,6 +9,8 @@ const About = () => {
             </div>
             <p className='text-xl'>
             I graduated from Cal Poly San Luis Obispo in 2019, then transitioned from Real Estate and Sales to software engineering. After completing Flatiron School's full-stack software engineering boot camp, I joined Pycube, Inc. as a software engineer intern and quickly secured a full-time position.</p>
+            <p className='text-xl mt-10'>
+           In my current role at Pycube, my primary focus revolves around the development of advanced solutions tailored specifically for the healthcare industry. This involves designing and implementing software that enable healthcare workers to effectively track and manage their diverse range of assets, ranging from medical equipment and facility resources, to invoice processing.</p>
                 <p className='text-xl mt-10'>
 I have proficiency in various languages and technologies, including: JavaScript, React, Redux, Toolkit, Node, Bootstrap, CSS (Tailwind, Material UI, & Bootstrap), Ruby, SQL, PostgreSQL Sinatra, REST APIs, Unit Teesting, Docker, JIRA, Figma, AWS (EC2, S3, Lambda), and pgAdmin.
                 </p>
