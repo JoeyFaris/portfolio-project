0ce588fd47121800006b8f54ba94e59251115502
Joey Faris <joeyfaris1@yahoo.com>
2024-05-02 16:59:02 -0700

    resume update

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -42,7 +42,7 @@ const SocialLinks = () => {
         Resume <BsFillPersonLinesFill size={30}/>
             </>
         ),
-        href:"/Joey-Faris-Resume.pdf",
+        href:"/Joey-Faris-Resume-3.pdf",
         style:'rounded-br-md',
         download: true,
     }
